5db18260d51010b104d920c83e3b8bf8c2c1017f
Joshua <blaqperformance@gmail.com>
2021-07-04 01:29:53 +0800

    completed most things, stuck on reading location in the background

--- lib/track_location.dart
+++ lib/track_location.dart
@@ -1,12 +1,19 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 class TrackLocation extends StatefulWidget {
+  
   @override
   _TrackLocationState createState() => _TrackLocationState();
 }
 
 class _TrackLocationState extends State<TrackLocation> {
+
+  var timer;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,12 +23,13 @@ class _TrackLocationState extends State<TrackLocation> {
       body: Center(
         child: Column(
           children: [
+            SizedBox(height: 30),
             TextButton(
-              onPressed: start, child: Text('Start')
+              onPressed: start, child: Text('Start Trip')
             ),
             SizedBox(height: 20),
             TextButton(
-              onPressed: stop, child: Text('Stop')
+              onPressed: stop, child: Text('End Trip')
             ),
           ],
         ),
@@ -53,13 +61,28 @@ class _TrackLocationState extends State<TrackLocation> {
         'Location permissions are permanently denied, we cannot request permissions.');
     } 
 
-    Position p = await Geolocator.getCurrentPosition();
+    int count = 0;
+    CollectionReference locations = FirebaseFirestore.instance.collection('locations');
 
-    print('Lat: '+p.latitude.toString());
-    print('Lng: '+p.longitude.toString());
+    timer = Timer.periodic(Duration(milliseconds: 5000), (timer) async { 
+      Position p = await Geolocator.getCurrentPosition();
+      count++;
+      print(count);
+
+      await locations.add({
+        'lat': p.latitude,
+        'lng': p.longitude,
+        'timestamp': DateTime.now().millisecondsSinceEpoch
+      });
+
+      print('Lat: '+p.latitude.toString());
+      print('Lng: '+p.longitude.toString());
+    });
+
   }
 
   void  stop() {
+    timer.cancel();
     print('stop tracking!');
   }
 }
